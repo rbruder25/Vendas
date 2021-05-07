6412c86eb754dd41a4a63ea8cc5872b4d23a3c3e
Roberto Bruder <robertobruder25@gmail.com>
2021-05-07 17:22:59 -0300

    Other entties and second migration

---
+++ Models/SalesRecord.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using SalesWebMvc.Models.Enums;
+
+namespace SalesWebMvc.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double  Amount { get; set; }
+        public SalesStatus Status { get; set; }
+        public Seller  Seller { get; set; } //Vendedor associação de muitos para um
+
+        public SalesRecord()
+        {
+
+        }
+
+        public SalesRecord(int id, DateTime date, double amount, SalesStatus status, Seller seller)
+        {
+            Id = id;
+            Date = date;
+            Amount = amount;
+            Status = status;
+            Seller = seller;
+        }
+    }
+}
